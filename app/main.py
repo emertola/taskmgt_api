aa78458ced2a3a4d9f27e91270d118cb31719a89
emertola <emertola@gmail.com>
2026-07-07 16:22:24 +0800

    feat: add configuration management with .env and pydantic-settings

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 
 from app.routers.tasks import router as tasks_router
+from app.core.config import settings
 
-app = FastAPI()
+app = FastAPI(
+  title=settings.app_name
+)
 
 app.include_router(tasks_router)
 
